81562577c451a85f518da876228e349e35c5b83a
HongChinhh <htrinhpadu@gmail.com>
2017-09-18 04:38:22 +0700

    showhelloworld
    use visual 2015 with window form to show helloword text

---
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(57, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 80);
+            this.label2.TabIndex = 2;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(171, 267);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(109, 23);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show Hello World";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(213, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 80);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Segoe Script", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloWorld.ForeColor = System.Drawing.Color.Red;
+            this.lblHelloWorld.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.lblHelloWorld.Location = new System.Drawing.Point(193, 76);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(34, 80);
+            this.lblHelloWorld.TabIndex = 3;
+            this.lblHelloWorld.Text = "\r\n";
+            this.lblHelloWorld.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHelloWorld.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 302);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmHelloWorld";
+            this.RightToLeftLayout = true;
+            this.Text = "Hello World";
+            this.Load += new System.EventHandler(this.ShowHelloWorld_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
